d4354fcdd05275be2a1e207fa9aa3bde73e8ff36
Javier Garcia Baroja <javier.garcia@unibe.ch>
2025-05-30 09:33:43 +0200

    correct typo

--- scripts/evaluate_metrics_mask2former_ln.py
+++ scripts/evaluate_metrics_mask2former_ln.py
@@ -150,7 +150,7 @@ def main(args):
             print(f'WSI for annotation {wsi_name} was not found')
 
     if args.apply_post_processing:
-        args.output_dir = os.path.join(args.output_dir, '_processed')
+        args.output_dir = args.output_dir + '_processed'
     # if no annotation files found, exit
     if len(ann_wsi_pairs) == 0:
         sys.exit('No annotation files found. Please check the path to annotation files and the WSI list')
